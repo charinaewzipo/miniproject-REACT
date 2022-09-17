32c51e27ccddd423b205399476cac15f143a141f
charin <charin.aewzipo@gmail.com>
2022-09-18 05:11:58 +0700

    make better

--- src/components/FoodComponent.js
+++ src/components/FoodComponent.js
@@ -1,9 +1,9 @@
-const FoodComponent = ({foodName,image_url}) =>{
+const FoodComponent = ({ foodName, image_url }) => {
   return (
     <div className="single-food">
-        <h2>{foodName}</h2>
-        <img src={image_url}></img>
+      <h2>{foodName}</h2>
+      <img src={image_url} alt=""></img>
     </div>
-  )
-}
-export default FoodComponent
+  );
+};
+export default FoodComponent;
