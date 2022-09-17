dcf6f9e136ed6148e6d8d515c29fc52a6266836f
charin <charin.aewzipo@gmail.com>
2022-09-18 05:30:13 +0700

    make better

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,17 +1,14 @@
-import { useEffect } from "react"
-const Alert=({msg,type,setAlert,list})=>{
-
+import { useEffect } from "react";
+const Alert = ({ msg, type, setAlert, list }) => {
   //ตั้งค่าให้alert 3วิ
-  useEffect(()=>{
-    const timeOut = setTimeout(()=>{
-      setAlert({show:false,msg:'',type:''})
-    },3000)
-    return()=>clearTimeout(timeOut)
-  },[list])
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert({ show: false, msg: "", type: "" });
+    }, 3000);
+    return () => clearTimeout(timeOut);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [list]);
 
-
-  return (
-    <p className={`alert ${type}`}>{msg}</p>
-  )
-}
-export default Alert
+  return <p className={`alert ${type}`}>{msg}</p>;
+};
+export default Alert;
