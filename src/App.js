1e2ac7bf8513aa3a947e3877449eaf396d407f23
charin <charin.aewzipo@gmail.com>
2022-09-18 05:01:48 +0700

    make better

--- src/App.js
+++ src/App.js
@@ -1,69 +1,68 @@
-import './App.css';
-import {useState,useEffect} from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
-  const [countries,setCountries]=useState([])
-  const apiUrl='https://restcountries.com/v2/all'
+  const [countries, setCountries] = useState([]);
+  const apiUrl = "https://restcountries.com/v2/all";
 
-  const [word,setWord]=useState('')
+  const [word, setWord] = useState("");
   //สนใจแค่nameกับcapital
-  const [dataFilter]=useState(["name","capital"])
+  const [dataFilter] = useState(["name", "capital"]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(apiUrl)
-    .then(res=>res.json())
-    .then(data=>{
-      setCountries(data)
-      console.log(data)
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+        console.log(data);
+      });
+  }, []);
 
-  const searchCountries=(countries)=>{
-    return countries.filter((item)=>{
-      return dataFilter.some((filter)=>{
-        if(item[filter]){
-          return item[filter].toLowerCase().indexOf(word.toLowerCase())>-1
-        }
-      })
-    })
-  }
+  const searchCountries = (countries) => {
+    return countries.filter((item) => {
+      return dataFilter.some((filter) => {
+        return item[filter]?.toLowerCase().indexOf(word.toLowerCase()) > -1;
+      });
+    });
+  };
 
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+  }
   return (
     <div className="container">
-      <div className='search-container'>
-        <label htmlFor='search-form'>
-          <input type="text" 
-          className="search-input" 
-          placeholder="ค้นหาประเทศที่ต้องการ" 
-          value={word}
-          onChange={(e)=>setWord(e.target.value)}
+      <div className="search-container">
+        <label htmlFor="search-form">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="ค้นหาประเทศที่ต้องการ"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
           ></input>
         </label>
       </div>
       <ul className="row">
-      {searchCountries(countries).map((item,index)=>{
-      return (
-        <li key={index}>
-          <div className='card'>
-            <div className='card-title'>
-              <img src={item.flag}/>
-            </div>
-            <div className='card-body'>
-              <div className='card-description'>
-                <h2>{item.name}</h2>
-                <ol className='card-list'>
-                  <li>ประชากร :{numberWithCommas(item.population)}</li>
-                  <li>ภูมิภาค :{item.region}</li>
-                  <li>เมืองหลวง :{item.capital}</li>
-                </ol>
+        {searchCountries(countries).map((item, index) => {
+          return (
+            <li key={index}>
+              <div className="card">
+                <div className="card-title">
+                  <img src={item.flag} />
+                </div>
+                <div className="card-body">
+                  <div className="card-description">
+                    <h2>{item.name}</h2>
+                    <ol className="card-list">
+                      <li>ประชากร :{numberWithCommas(item.population)}</li>
+                      <li>ภูมิภาค :{item.region}</li>
+                      <li>เมืองหลวง :{item.capital}</li>
+                    </ol>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </li>
-      )
-      })}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
